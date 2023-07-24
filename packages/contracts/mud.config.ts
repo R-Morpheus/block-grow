2e1e7787703141e63005d9f0b239c7f9adbbe440
rashid <gagach16rus@gmail.com>
2023-07-24 21:28:26 +0300

    add: first structure, tables

--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -1,10 +1,57 @@
 import { mudConfig } from "@latticexyz/world/register";
 
+const EntityId = "bytes32" as const
+const EntityIdArray = "bytes32[]" as const
+const EntityIdSet = "bytes32[]" as const
+
+const entityKey = {
+  primaryKeys: {
+    entity: EntityId,
+  },
+} as const
+
+const entityRelation = {
+  ...entityKey,
+  schema: EntityId,
+} as const
+
 export default mudConfig({
   tables: {
     Counter: {
       keySchema: {},
       schema: "uint32",
+    },
+    User:{
+      keySchema: {},
+      schema: EntityId,
+    },
+    Blockchain:{
+      keySchema: {},
+      schema: EntityId,
+    },
+    Block:{
+      keySchema: {},
+      schema: EntityId,
+    },
+    Transaction:{
+      keySchema: {},
+      schema: EntityId,
+    },
+    CryptoCurrency:{
+      keySchema: {},
+      schema: EntityId,
+    },
+    MiningEquipment:{
+      keySchema: {},
+      schema: EntityId,
+    },
+    Market:{
+      keySchema: {},
+      schema: EntityId,
+    },
+    Event:{
+      keySchema: {},
+      schema: EntityId,
     },
   },
 });
