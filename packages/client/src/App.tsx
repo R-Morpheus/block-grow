f09d98d17cc9ecfe5f3387c156a7df6e66fa2188
rashid <gagach16rus@gmail.com>
2023-07-26 23:22:23 +0300

    add: startPage

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,29 +1,10 @@
-import { useComponentValue } from "@latticexyz/react";
-import { useMUD } from "./MUDContext";
+import StartPage from "./pages/StartPage/StartPage";
 
 export const App = () => {
-  const {
-    components: { Counter },
-    systemCalls: { increment },
-    network: { singletonEntity },
-  } = useMUD();
-
-  const counter = useComponentValue(Counter, singletonEntity);
 
   return (
     <>
-      <div>
-        Counter: <span>{counter?.value ?? "??"}</span>
-      </div>
-      <button
-        type="button"
-        onClick={async (event) => {
-          event.preventDefault();
-          console.log("new counter value:", await increment());
-        }}
-      >
-        Increment
-      </button>
-    </>
+      <StartPage/>
+      </>
   );
 };
